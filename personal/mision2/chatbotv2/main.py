99e33afa52226586b6452ca56cf0c03c62e2ad03
Manuel <zapatamanuel.1998@gmail.com>
2025-11-11 11:31:41 -0500

    chatbootv2

--- personal/mision2/chatbotv2/main.py
+++ personal/mision2/chatbotv2/main.py
@@ -1,9 +1,9 @@
 from chatbot.data import training_data
 from chatbot.model import buid_and_train_model, predict_answer, load_model
 def main():
-    model,vectorizer,unique_answers=load_model()
-    if model in None:
-        model,vectorizer,unique_answers, buid_and_train_model(training_data)
+    model, vectorizer, unique_answers = load_model()
+    if model is None:
+        model, vectorizer, unique_answers, buid_and_train_model(training_data)
     print("\n 🤖 chatbot listo. Escribe salir para terminar. \n")
     while True:
         user = input("tu: ").strip()
@@ -12,6 +12,6 @@ def main():
             break
         response = predict_answer(model, vectorizer, unique_answers, user)
         print("bot:", response)
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     
